c60ad161bc68f6e7f9dac4753852f659617eabf9
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-21 16:10:31 +0200

    added functionality of displaying lyrics after clicking specify button

--- script.js
+++ script.js
@@ -46,6 +46,21 @@ async function getMoreSongs(url) {
     showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `
+        <h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>
+    `;
+
+    more.innerHTML = '';
+}
+
 // Event listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -64,7 +79,7 @@ form.addEventListener('submit', e => {
 result.addEventListener('click', e => {
     const clickedEl = e.target;
 
-    if(clickedEl.tagName === 'BUTTON') {
+    if (clickedEl.tagName === 'BUTTON') {
         const artist = clickedEl.getAttribute('data-artist');
         const songTitle = clickedEl.getAttribute('data-songtitle');
 
