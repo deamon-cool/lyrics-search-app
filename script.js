c02db52c7eed5b03cba62f62fa98e0a90efbd4ef
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-21 11:09:06 +0200

    fetch data from api

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
+// Search by song artists
+function searchSong(term) {
+    fetch(`${apiURL}/suggest/${term}`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+
 // Event listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    const searchTrim = search.value.trim();
+    const searchTerm = search.value.trim();
+
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSong(searchTerm);
+    }
 
 });
