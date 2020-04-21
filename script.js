12fc37dff540397ae7487985e5271653370318d1
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-21 14:14:04 +0200

    finished function

--- script.js
+++ script.js
@@ -15,20 +15,28 @@ async function searchSong(term) {
 
 // Show song and artists in DOM
 function showData(data) {
-
-    return result.innerHTML = `
+    result.innerHTML = `
         <ul class="songs">
-            ${data.data.map(song =>
-                `<li>
+            ${data.data.map(song => `<li>
                 <span><strong>${song.artist.name}</strong> - ${song.title}</span>
                 <button class="btn" data-artist="${song.artist.name}"
                 data-songtitle="${song.title}">Get Lyrics</button>
                 </li>`
-            ).join('')}
+    ).join('')}
         </ul>
     `;
-}
 
+    if (data.prev || data.next) {
+        more.innerHTML = `
+            ${data.prev ? `<button class="btn"
+            onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+            ${data.next ? `<button class="btn"
+            onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+        `;
+    } else {
+        more.innerHTML = '';
+    }
+}
 
 // Event listeners
 form.addEventListener('submit', e => {
